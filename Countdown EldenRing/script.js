eb3732a373ca027c9fd8d4008f738dede9deb414
Alexandru Tabara <89846474+AlexTab1@users.noreply.github.com>
2021-09-02 14:10:08 +0300

    Add files via upload

---
+++ Countdown EldenRing/script.js
@@ -0,0 +1,35 @@
+// end date 21 Jan 2022
+const daysEl =  document.getElementById('days');    
+const hoursEl = document.getElementById('hours');   
+const minsEl = document.getElementById('mins'); 
+const secondsEl =  document.getElementById('seconds');
+
+
+const newRelease = "21 Jan 2022";
+
+
+function countdown() {
+    const newReleaseDate  = new Date(newRelease);
+    const currentDate = new Date();
+
+    const totalSeconds = (newReleaseDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60; 
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
